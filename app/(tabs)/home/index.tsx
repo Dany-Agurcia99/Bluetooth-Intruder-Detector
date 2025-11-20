cb5b14012dfc9675ac48d69e991f2d6fc924f3d3
danie <danielagurcia90@gmail.com>
2025-11-19 18:06:14 -0600

    feat: implement BLE scanning with permission handling and update device list rendering

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -2,32 +2,76 @@ import ScreenWrapper from "@/components/ScreenWrapper"
 import ScanButton from "./components/ScanButton"
 import DeviceList from "./components/DeviceList"
 import { useState } from "react"
-import Device from "@/types/device"
+import DeviceType from "@/types/device"
+import { BleManager, Device } from "react-native-ble-plx"
+import { Platform, PermissionsAndroid } from "react-native"
 
-const mockDevices: Device[] = [
-  { id: "AA:BB:CC:DD:EE:01", name: "iPhone 13", rssi: -45 },
-  { id: "AA:BB:CC:DD:EE:02", name: "Samsung Galaxy", rssi: -67 },
-  { id: "AA:BB:CC:DD:EE:03", name: "MacBook Pro", rssi: -32 },
-  { id: "AA:BB:CC:DD:EE:04", name: "AirPods Pro", rssi: -78 },
-  { id: "AA:BB:CC:DD:EE:05", name: "Unknown Device", rssi: -89 },
-  { id: "AA:BB:CC:DD:EE:06", name: "Apple Watch", rssi: -56 },
-]
+const manager = new BleManager()
 
 const HomeScreenLayout = () => {
   const [isScanning, setIsScanning] = useState(false)
+  const [devices, setDevices] = useState<DeviceType[]>([])
+
+  async function requestBlePermissions() {
+    if (Platform.OS === "android") {
+      const granted = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      ])
+
+      const allGranted = Object.values(granted).every(
+        (result) => result === PermissionsAndroid.RESULTS.GRANTED
+      )
+
+      return allGranted
+    }
+    return true
+  }
 
   const handleScan = async () => {
     if (isScanning) return
-    console.log("Scanning...")
+    const ok = await requestBlePermissions()
+    if (!ok) {
+      console.warn("Permissions denied")
+      return
+    }
+    setDevices([])
     setIsScanning(true)
+
+    console.log("Scanning...")
+
+    manager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.log("Scan error:", error)
+        return
+      }
+
+      if (device) {
+        setDevices((prev) => {
+          const exists = prev.find((d) => d.id === device.id)
+          if (exists) return prev
+
+          const deviceData: DeviceType = {
+            id: device.id,
+            name: device.name || "Unknown Device",
+            rssi: device.rssi || -100,
+          }
+          return [...prev, deviceData]
+        })
+      }
+    })
+
     setTimeout(() => {
+      manager.stopDeviceScan()
       setIsScanning(false)
+      console.log("Stopped Scanning")
     }, 3000)
   }
 
   return (
     <ScreenWrapper>
-      <DeviceList devices={mockDevices} />
+      <DeviceList devices={devices} />
       <ScanButton onScan={handleScan} isScanning={isScanning} />
     </ScreenWrapper>
   )
